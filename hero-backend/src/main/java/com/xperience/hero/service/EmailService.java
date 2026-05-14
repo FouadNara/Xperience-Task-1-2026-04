3ca35bac9933325687d9763f86b91382970e7231
FouadNara <fznara@gmail.com>
2026-05-14 15:25:55 +0300

    fixed unused variables

--- hero-backend/src/main/java/com/xperience/hero/service/EmailService.java
+++ hero-backend/src/main/java/com/xperience/hero/service/EmailService.java
@@ -14,16 +14,6 @@ public class EmailService {
     
     public void sendInvitationEmail(Invitation invitation) {
         try {
-            String subject = "You're invited to " + invitation.getEvent().getTitle();
-            String rsvpLink = "http://localhost:3000/rsvp/" + invitation.getToken();
-            String body = "Hello,\n\n" +
-                "You are invited to: " + invitation.getEvent().getTitle() + "\n" +
-                "Date: " + invitation.getEvent().getDateTime() + "\n" +
-                "Location: " + invitation.getEvent().getLocation() + "\n\n" +
-                "Please respond to this invitation by clicking the link below:\n" +
-                rsvpLink + "\n\n" +
-                "Thank you!";
-            
             // In production, integrate with actual email service
             logger.info("Sending invitation email to {} for event {}", 
                 invitation.getInviteeEmail(), invitation.getEvent().getTitle());
@@ -40,15 +30,6 @@ public class EmailService {
     
     public void sendConfirmationEmail(Invitation invitation, RSVP rsvp) {
         try {
-            String subject = "RSVP Confirmation - " + invitation.getEvent().getTitle();
-            String body = "Hello,\n\n" +
-                "Your RSVP response has been recorded.\n" +
-                "Status: " + rsvp.getStatus() + "\n\n" +
-                "Event: " + invitation.getEvent().getTitle() + "\n" +
-                "Date: " + invitation.getEvent().getDateTime() + "\n" +
-                "Location: " + invitation.getEvent().getLocation() + "\n\n" +
-                "Thank you!";
-            
             logger.info("Sending confirmation email to {} for event {}", 
                 invitation.getInviteeEmail(), invitation.getEvent().getTitle());
             
@@ -59,15 +40,6 @@ public class EmailService {
     
     public void sendWaitlistPromotionEmail(Invitation invitation, RSVP rsvp) {
         try {
-            String subject = "You're confirmed! - " + invitation.getEvent().getTitle();
-            String body = "Great news!\n\n" +
-                "A spot has opened up and you've been promoted from the waitlist!\n" +
-                "Your new status: CONFIRMED\n\n" +
-                "Event: " + invitation.getEvent().getTitle() + "\n" +
-                "Date: " + invitation.getEvent().getDateTime() + "\n" +
-                "Location: " + invitation.getEvent().getLocation() + "\n\n" +
-                "See you there!";
-            
             logger.info("Sending waitlist promotion email to {}", invitation.getInviteeEmail());
             
         } catch (Exception e) {
@@ -77,13 +49,6 @@ public class EmailService {
     
     public void sendEventCancellationEmail(String inviteeEmail, String eventTitle) {
         try {
-            String subject = "Event Cancelled - " + eventTitle;
-            String body = "Hello,\n\n" +
-                "The following event has been cancelled:\n" +
-                "Event: " + eventTitle + "\n\n" +
-                "We apologize for any inconvenience.\n\n" +
-                "Thank you!";
-            
             logger.info("Sending cancellation email to {}", inviteeEmail);
             
         } catch (Exception e) {
